198f2260720b0ce2225322210dfaf8ebaf2c6028
Raouf Yasin <babaraoufyasin111@hotmail.com>
2021-03-28 14:09:48 +0100

    Add project files.

---
+++ PersonalInformationSystem/Screens/DashboardScreen.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace PersonalInformationSystem.Screens
+{
+    partial class DashboardScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DashboardScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 687);
+            this.Name = "DashboardScreen";
+            this.Text = "DashboardScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ PersonalInformationSystem/Screens/DashboardScreen.cs
@@ -0,0 +1,21 @@
+﻿using PersonalInformationSystem.TemplateScreens;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalInformationSystem.Screens
+{
+    public partial class DashboardScreen : BaseTemplateScreen
+    {
+        public DashboardScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
